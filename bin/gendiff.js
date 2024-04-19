9567a009872787d6d856f064cf704b41378a3f51
Benamgkhar <swsbbnml@gmail.com>
2024-04-19 17:24:11 +0300

    19.04.2024 - test

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../test.js';
+
 
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.1')
+    .option('-f, --format [type]', 'output format')
     .argument('<filepath1>')
     .argument('<filepath2>')
-    .option('-f, --format [type]', 'output format');
+    .action((filePath1, filePath2) => genDiff(filePath1, filePath2));
 program.parse();
+
