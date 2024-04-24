0f7ea4396d3c4ce1ebad5d6d7978e4aef8afa480
Benamgkhar <swsbbnml@gmail.com>
2024-04-24 21:43:01 +0300

    -

--- reader.js
+++ reader.js
@@ -31,29 +31,35 @@ const getSorted = (values) => {
 };
 
 const getUniqKeys = (dataA, dataB) => _.uniq([..._.keys(dataA), ..._.keys(dataB)]);
+
+const getResult = (keys, dataA, dataB) => {
+  const result = keys.flatMap((key) => {
+    const diffArr = [];
+    if (_.has(dataA, key) && _.has(dataB, key)) {
+      if (dataA[key] === dataB[key]) {
+        diffArr.push(`   ${key}: ${dataA[key]}`);
+      }
+      if (dataA[key] !== dataB[key]) {
+        diffArr.push(` - ${key}: ${dataA[key]}`);
+        diffArr.push(` + ${key}: ${dataB[key]}`);
+      }
+    }
+    if (_.has(dataA, key) && !_.has(dataB, key)) {
+      diffArr.push(` - ${key}: ${dataA[key]}`);
+    }
+    if (!_.has(dataA, key) && _.has(dataB, key)) {
+      diffArr.push(` + ${key}: ${dataB[key]}`);
+    }
+    return diffArr;
+  });
+  return result;
+};
 //--------------------------------------
 
 const getDiff = (fstFileData, scndFileData) => {
   const uniqKeys = getUniqKeys(fstFileData, scndFileData);
-  const result = uniqKeys.flatMap((key) => {
-    const diffArr = [];
-    if (_.has(fstFileData, key) && _.has(scndFileData, key)) {
-      if (fstFileData[key] === scndFileData[key]) {
-        diffArr.push(`   ${key}: ${fstFileData[key]}`);
-      }
-      if (fstFileData[key] !== scndFileData[key]) {
-        diffArr.push(` - ${key}: ${fstFileData[key]}`);
-        diffArr.push(` + ${key}: ${scndFileData[key]}`);
-      }
-    }
-    if (_.has(fstFileData, key) && !_.has(scndFileData, key)) {
-      diffArr.push(` - ${key}: ${fstFileData[key]}`);
-    }
-    if (!_.has(fstFileData, key) && _.has(scndFileData, key)) {
-      diffArr.push(` + ${key}: ${scndFileData[key]}`);
-    }
-    return diffArr;
-  });
+
+  const result = getResult(uniqKeys, fstFileData, scndFileData);
   const sortedResult = getSorted(result);
 
   return `{\n${sortedResult.join('\n')}\n}`;
