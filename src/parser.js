663af13eff557fdac59167b54e6452de2029973e
Benamgkhar <swsbbnml@gmail.com>
2024-05-10 01:45:38 +0300

    test3

--- src/parser.js
+++ src/parser.js
@@ -11,6 +11,6 @@ export default (fileData, fileType) => {
       return JSON.parse(toJson);
     }
     default:
-      throw Error(`Format ${fileformat} is not supported!`);
+      throw Error(`Format ${fileType} is not supported!`);
   }
 };
