708fabcc497a96d38af03ea21e8c5fb016424978
Benamgkhar <swsbbnml@gmail.com>
2024-05-09 13:00:40 +0300

    #5

--- src/results-builder.js
+++ src/results-builder.js
@@ -2,27 +2,27 @@ import _ from 'lodash';
 import { getSorted } from './helper.js';
 import formater from './formaters/index.js';
 
-const getDiffInfo = (dataA, dataB) => {
-  const keys1 = Object.keys(dataA);
-  const keys2 = Object.keys(dataB);
+const getDiffInfo = (firstData, seconData) => {
+  const keys1 = Object.keys(firstData);
+  const keys2 = Object.keys(seconData);
   const keys = _.sortBy(_.union(keys1, keys2));
 
   return keys.map((key) => {
-    if (_.isPlainObject(dataA[key]) && _.isPlainObject(dataB[key])) {
-      return { name: key, type: 'nested', value: getDiffInfo(dataA[key], dataB[key]) };
+    if (_.isPlainObject(firstData[key]) && _.isPlainObject(seconData[key])) {
+      return { name: key, type: 'nested', value: getDiffInfo(firstData[key], seconData[key]) };
     }
-    if (!Object.hasOwn(dataB, key)) {
-      return { name: key, type: 'deleted', value: dataA[key] };
+    if (!Object.hasOwn(seconData, key)) {
+      return { name: key, type: 'deleted', value: firstData[key] };
     }
-    if (!Object.hasOwn(dataA, key)) {
-      return { name: key, type: 'added', value: dataB[key] };
+    if (!Object.hasOwn(firstData, key)) {
+      return { name: key, type: 'added', value: seconData[key] };
     }
-    if (dataA[key] !== dataB[key]) {
+    if (firstData[key] !== seconData[key]) {
       return {
-        name: key, type: 'changed', value1: dataA[key], value2: dataB[key],
+        name: key, type: 'changed', value1: firstData[key], value2: seconData[key],
       };
     }
-    return { name: key, type: 'unchanged', value: dataA[key] };
+    return { name: key, type: 'unchanged', value: firstData[key] };
   });
 };
 
